2fd67a0a2d6f0557c469e5532c550d7ebc060332
EDUARDO SALOMÃO DE OLIVEIRA JARDIM <eduardo.jardim@a.ucb.br>
2023-08-11 10:41:53 -0300

    VerificaNumeroPrimo.Java

---
+++ atividade1/VerificaNumeroPrimo.java
@@ -0,0 +1,16 @@
+package atividade1;
+
+/**
+ * VerificaNumeroPrimo
+ */
+public class VerificaNumeroPrimo {
+
+    public static void main(String args[]){
+        System.out.println("Ola mundo");
+    }
+
+    public void verificarNumeroPrimo(Integer n){
+        
+    }
+    
+}
